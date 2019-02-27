650b4ce0b4b29c00abbd1fbbe80816b4ea52ecb8
Taylor Savage <taylorsavage@DN0a62e451.SUNet>
2019-02-26 19:12:49 -0800

    set up 5-fold structure

--- predict.R
+++ predict.R
@@ -1,36 +1,60 @@
+#### SETUP CODE ####
 rm(list = ls())
 
 source("http://www.stanford.edu/~bayati/oit367/T367_utilities_13_beta.R")
+library(rstudioapi)
 
-this.dir <- dirname(parent.frame(2)$ofile)
-setwd(this.dir)
+current_path <- getActiveDocumentContext()$path 
+setwd(dirname(current_path ))
 
 training = read.csv("training.csv")
-
 test = read.csv("test.csv")
-
 training$Fund_Date = NULL # Removing this column
-
 test$accepted = rep(0,nrow(test)) # Adding a dummy response variable for the test set
-
 alldata = rbind(training,test)  # Combining the two data frames to preprocess them together
-
 alldata$Id = NULL  # Removing the Id column since it should not be used for model building
-
-# Column Previous_Rate has missing values, a basic preprocessing would be to remove it
-
-alldata$Previous_Rate = NULL 
-
+alldata$Previous_Rate = NULL # Column Previous_Rate has missing values, a basic preprocessing would be to remove it
 
 trainingProcessed = alldata[1:nrow(training),]
 testProcessed = alldata[-(1:nrow(training)),]
 
-model = glm(accepted ~ Amount_Approved + CarType, data = trainingProcessed, family = "binomial")
+## Randomize the training data, and split it into a validation set and a new, smaller training set
 
+trainingProcessedRandomized = trainingProcessed[sample(nrow(trainingProcessed)),]
+
+num_folds = 5
+folds <- cut(seq(1,nrow(trainingProcessedRandomized)),breaks=num_folds,labels=FALSE)
+
+#### END SETUP CODE ####
+
+#### Train the model ####
+
+# Create a function in which we pass in the training data set, and return the model
+generateModel <- function(trainingSet) {
+  model = glm(accepted ~ Amount_Approved + CarType, data = trainingSet, family = "binomial")
+  
+  return(model)
+}
+
+# Do a 5-fold cross-validation on the model
+
+for(i in 1:num_folds) {
+  # Set up the current validation and training sets using the folds
+  currValidationIndexes <- which(folds==i, arr.ind=TRUE)
+  currValidationSet <- trainingProcessedRandomized[currValidationIndexes,]
+  currTrainingSet <- trainingProcessedRandomized[-currValidationIndexes,]
+  
+  # Train the model
+  currModel <- generateModel(currTrainingSet)
+  currPredictions <- predict(currModel, newdata=currValidationSet)
+  cat(auc(currValidationSet$accepted, currPredictions) + "\n")
+}
+
+
+#### Output predictions for the test set
 probabilities = predict(model, newdata = testProcessed, type = "response")
-
 submission = data.frame(Id=test$Id, Prediction = probabilities)
-
 write.csv(submission, file = "benchmark.csv", row.names = FALSE)
 
-endTime=proc.time()[3]  # records current time to calculate overall code's run-time
+
+cat("AUC: " + auc())
